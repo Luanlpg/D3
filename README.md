ac6c65e2f621094de64c3c2dba4f947ea01495bd
Luan Santos <501526@soa.ad.pwcinternal.com>
2020-01-29 19:14:00 -0300

    web crawler

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-`Python run.py`
+# Web Crawler / Logic test
+
+## Instalação de requisitos (NECESSÁRIO:Python 3.6.0+ e pip(Atualizado))
+
+- Crie um ambiente virtual de sua preferência (Recomendado/Opcional)
+
+- Faça um clone do projeto: `git clone https://github.com/Luanlpg/D3.git`
+
+- Acesse o repositório: `cd D3/`
+
+- Faça a instalação do `requirements.txt` usando o comando: `pip install -r requirements.txt`
+
+
+## Rodando
+
+- Rode o teste de lógica: `Python run_logic_test.py`
+
+- Rode o Web Crawler: `scrapy runspider scraper.py`
+
+  - Verifique a resposta na pasta responses.
---
+++ scraper.py
@@ -0,0 +1,57 @@
+import scrapy
+import time
+import json
+
+
+class ElixirLangSpider(scrapy.Spider):
+    name = "elixir_spider"
+    start_urls = ['https://elixir-lang.org']
+
+    def __init__(self):
+        self.document = []
+        self.file_name = f'responses/{time.time()}.json'
+
+    def parse(self, response):
+        """=====================================================================
+        Método que chama o parser de cada tela do site.
+        ====================================================================="""
+        # seletor de menu
+        MENU_SELECTOR = '.menu-item'
+        for item in response.css(MENU_SELECTOR):
+            # crio e envio urls para fazer requisição GET
+            HREF_SELECTOR = 'li a::attr(href)'
+            yield self.get_secundary_page(response.urljoin(item.css(HREF_SELECTOR).extract_first()))
+
+    def get_secundary_page(self, url):
+        """=====================================================================
+        Método que faz requisição GET em url.
+        ====================================================================="""
+        return scrapy.Request(url, callback=self.register)
+
+    def register(self, response):
+        """=====================================================================
+        Método que parsea e registra informações estaticas de cada tela.
+        ====================================================================="""
+        # seletores
+        title = response.css('title ::text').extract_first()
+        css = response.xpath('//link[@rel="stylesheet"]').css('::attr(href)').getall()
+        js = response.css('script::attr(src)').getall()
+        img = response.css('img::attr(src)').getall()
+        # adiciono registrom em resposta
+        self.document.append(
+            {
+                "title": title,
+                "css": css,
+                "js": js,
+                "img": img
+            }
+        )
+        self.create_json()
+
+    def create_json(self):
+        """=====================================================================
+        Método que cria json de resposta na pasta responses.
+        ====================================================================="""
+        arq = open(self.file_name, 'w')
+        arq.write(json.dumps(self.document))
+        arq.close()
